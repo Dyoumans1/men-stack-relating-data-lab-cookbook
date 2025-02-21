367250b48bd69095192f4635e539d4caff9e9c5b
Dyoumans <derrikyoumans416@gmail.com>
2025-02-20 23:07:55 -0500

    Added show page and made the routes to make ingredients

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js');
-const Ingredients = require('../models/ingredient.js');
+const Ingredient = require('../models/ingredient.js');
 
+router.get('/', async (req, res) => {
+    const ingredients = await Ingredient.find({});
+    res.render('ingredients/index.ejs', { ingredients });     
+});
+
+router.post('/', async (req, res) => {
+    await Ingredient.create(req.body);
+    res.redirect('/ingredients');
+});
 
 
 module.exports = router;
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,6 +3,38 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 const Recipe = require('../models/recipe.js');
+const Ingredient = require('../models/ingredient.js')
+
+
+ router.get('/', async (req, res) => {
+    try {
+        const recipes = await Recipe.find({ owner: req.session.user._id });
+        res.render('recipes/index.ejs', { recipes });
+     } catch (err) {
+        console.log(err);
+        res.redirect('/');
+        }
+    });
+
+
+router.get('/new', async (req, res) => {
+    const ingredients = await Ingredient.find({});
+    res.render('recipes/new.ejs', {ingredients});
+});
+
+router.post('/', async (req, res) => {
+        const recipe = new Recipe(req.body);
+        recipe.owner = req.session.user._id;
+        await recipe.save();
+        res.redirect('/recipes');
+});
+
+router.get('/:id', async (req, res) => {
+    const recipe = await Recipe.findById(req.params.id);
+    res.render('recipes/show.ejs', { recipe });
+});
+
+
 
 
 
